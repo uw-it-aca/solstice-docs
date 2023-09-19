7cee112e337a9a7b65069874ff19d3526bdb70d0
Charlon Palacay <charlon@gmail.com>
2023-09-19 14:09:42 -0700

    Updates variants display.

--- src/pages/elements/link.vue
+++ src/pages/elements/link.vue
@@ -15,65 +15,70 @@
       <hr class="mb-5 w-25 d-inline-block" />
 
       <h2>Variants</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed dicta
-        accusamus ipsa saepe! Earum nisi corporis minima consequatur ut
-        accusantium quos expedita natus illo quisquam laudantium ipsa eum,
-        aspernatur explicabo.
-      </p>
 
-      <h3 class="h5">Default</h3>
-      <p>
-        Default links are those found within sentences or paragraphs, surrounded
-        by unlinked text. Body links are always styled with an underline and
-        appear in blue by default, with light and dark variations. This helps
-        differentiate them from surrounding text and makes it clear that users
-        can interact with them. Body links should not be used on their own
-        (i.e., outside of sentences and paragraphs); for this purpose, see Quiet
-        links.
-      </p>
+      <div class="row">
+        <div class="col">
+          <h3 class="h5">Default</h3>
+          <p>
+            Default links are those found within sentences or paragraphs,
+            surrounded by unlinked text. Body links are always styled with an
+            underline and appear in blue by default, with light and dark
+            variations. This helps differentiate them from surrounding text and
+            makes it clear that users can interact with them. Body links should
+            not be used on their own (i.e., outside of sentences and
+            paragraphs); for this purpose, see Quiet links.
+          </p>
 
-      <p><sol-link href="https://www.google.com">google.com</sol-link></p>
+          <p><sol-link href="https://www.google.com">google.com</sol-link></p>
+        </div>
+        <div class="col">
+          <h3 class="h5">Quiet</h3>
+          <p>
+            Quiet links are standalone links (i.e., not contained within a
+            sentence or paragraph) that most often appear in a group with other
+            links, such as in lists of “quick links,” sidebars, footers, or
+            similar navigation elements. Quiet links may be grouped together
+            horizontally or vertically. They are styled without an underline
+            (making them “quiet”) and appear in blue by default, with light and
+            dark variations.
+          </p>
 
-      <h3 class="h5">Quiet</h3>
-      <p>
-        Quiet links are standalone links (i.e., not contained within a sentence
-        or paragraph) that most often appear in a group with other links, such
-        as in lists of “quick links,” sidebars, footers, or similar navigation
-        elements. Quiet links may be grouped together horizontally or
-        vertically. They are styled without an underline (making them “quiet”)
-        and appear in blue by default, with light and dark variations.
-      </p>
-
-      <p>
-        <sol-link href="https://www.google.com" variant="quiet"
-          >google.com</sol-link
-        >
-      </p>
-
-      <h3 class="h5">Icon</h3>
-      <p>
-        By default, Quiet links appear only as text. However, there may be some
-        cases where the inclusion of an icon enhances the user experience by
-        making it easier for users to understand the purpose or destination of
-        the link. For example, a small envelope icon next to a link might
-        indicate an email contact, a download icon could represent a file
-        download link, or an external link icon might signify that clicking the
-        link will take you to an external website. Icons should be used
-        sparingly and only when it provides true value to the interface.
-      </p>
-      <p>
-        When an icon is used with a link that is part of a collection of links,
-        the links should all match each other in terms of the inclusion of an
-        icon; either all of the links should have accompanying icons, or none of
-        them should.
-      </p>
-      <p>Icons should not be used with Body links or Logos as links.</p>
-      <p>
-        <sol-link href="http://www.google.com" variant="icon" icon="bi-android2"
-          >aksdfjaslkdfj</sol-link
-        >
-      </p>
+          <p>
+            <sol-link href="https://www.google.com" variant="quiet"
+              >google.com</sol-link
+            >
+          </p>
+        </div>
+        <div class="col">
+          <h3 class="h5">Icon</h3>
+          <p>
+            By default, Quiet links appear only as text. However, there may be
+            some cases where the inclusion of an icon enhances the user
+            experience by making it easier for users to understand the purpose
+            or destination of the link. For example, a small envelope icon next
+            to a link might indicate an email contact, a download icon could
+            represent a file download link, or an external link icon might
+            signify that clicking the link will take you to an external website.
+            Icons should be used sparingly and only when it provides true value
+            to the interface.
+          </p>
+          <p>
+            When an icon is used with a link that is part of a collection of
+            links, the links should all match each other in terms of the
+            inclusion of an icon; either all of the links should have
+            accompanying icons, or none of them should.
+          </p>
+          <p>Icons should not be used with Body links or Logos as links.</p>
+          <p>
+            <sol-link
+              href="http://www.google.com"
+              variant="icon"
+              icon="bi-android2"
+              >aksdfjaslkdfj</sol-link
+            >
+          </p>
+        </div>
+      </div>
 
       <h2>Anatomy</h2>
       <h3>Color</h3>
