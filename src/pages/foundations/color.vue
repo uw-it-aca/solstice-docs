c55cb3f686ef0c921b1d9d75831d4081f60081e8
Charlon Palacay <charlon@gmail.com>
2024-01-09 16:05:56 -0800

    Additional historic swatch.

--- src/pages/foundations/color.vue
+++ src/pages/foundations/color.vue
@@ -231,16 +231,16 @@
       </p>
 
       <h4>Solstice Colors &amp; Swatches</h4>
-      <div class="d-flex w-50">
-        <div style="width: 150px" class="col mb-3">
+      <div class="grid" style="--bs-columns: 6">
+        <div>
           <div class="p-3 mb-2 position-relative bg-purple2-500 text-white">
             <strong class="d-block">$purple2</strong>
             #4b2e83
           </div>
-          <div class="p-3 bg-purple2-100">$purple2-100</div>
-          <div class="p-3 bg-purple2-200">$purple2-200</div>
-          <div class="p-3 bg-purple2-300">$purple2-300</div>
-          <div class="p-3 bg-purple2-400 text-light">$purple2-400</div>
+          <div class="p-3 bg-purple2-100 text-black">$purple2-100</div>
+          <div class="p-3 bg-purple2-200 text-black">$purple2-200</div>
+          <div class="p-3 bg-purple2-300 text-black">$purple2-300</div>
+          <div class="p-3 bg-purple2-400 text-white">$purple2-400</div>
           <div class="p-3 bg-purple2-500 text-white">$purple2-500</div>
           <div class="p-3 bg-purple2-600 text-white">$purple2-600</div>
           <div class="p-3 bg-purple2-700 text-white">$purple2-700</div>
@@ -248,15 +248,15 @@
           <div class="p-3 bg-purple2-900 text-white">$purple2-900</div>
         </div>
 
-        <div style="width: 150px" class="col mb-3">
+        <div>
           <div class="p-3 mb-2 position-relative bg-red2-500 text-white">
             <strong class="d-block">$red2</strong>
             #cb444a
           </div>
-          <div class="p-3 bg-red2-100">$red2-100</div>
-          <div class="p-3 bg-red2-200">$red2-200</div>
-          <div class="p-3 bg-red2-300">$red2-300</div>
-          <div class="p-3 bg-red2-400 text-light">$red2-400</div>
+          <div class="p-3 bg-red2-100 text-black">$red2-100</div>
+          <div class="p-3 bg-red2-200 text-black">$red2-200</div>
+          <div class="p-3 bg-red2-300 text-black">$red2-300</div>
+          <div class="p-3 bg-red2-400 text-black">$red2-400</div>
           <div class="p-3 bg-red2-500 text-white">$red2-500</div>
           <div class="p-3 bg-red2-600 text-white">$red2-600</div>
           <div class="p-3 bg-red2-700 text-white">$red2-700</div>
@@ -264,20 +264,52 @@
           <div class="p-3 bg-red2-900 text-white">$red2-900</div>
         </div>
 
-        <div style="width: 150px" class="col mb-3">
+        <div>
           <div class="p-3 mb-2 position-relative bg-blue2-500 text-white">
             <strong class="d-block">$blue2</strong>
-            #2F68CB
+            #2f68cb
           </div>
-          <div class="p-3 bg-blue2-100">$blue2-100</div>
-          <div class="p-3 bg-blue2-200">$blue2-200</div>
-          <div class="p-3 bg-blue2-300">$blue2-300</div>
-          <div class="p-3 bg-blue2-400 text-light">$blue2-400</div>
+          <div class="p-3 bg-blue2-100 text-black">$blue2-100</div>
+          <div class="p-3 bg-blue2-200 text-black">$blue2-200</div>
+          <div class="p-3 bg-blue2-300 text-black">$blue2-300</div>
+          <div class="p-3 bg-blue2-400 text-black">$blue2-400</div>
           <div class="p-3 bg-blue2-500 text-white">$blue2-500</div>
           <div class="p-3 bg-blue2-600 text-white">$blue2-600</div>
           <div class="p-3 bg-blue2-700 text-white">$blue2-700</div>
           <div class="p-3 bg-blue2-800 text-white">$blue2-800</div>
           <div class="p-3 bg-blue2-900 text-white">$blue2-900</div>
+        </div>
+
+        <div>
+          <div class="p-3 mb-2 position-relative bg-beige-500 text-black">
+            <strong class="d-block">$beige</strong>
+            #ab9765
+          </div>
+          <div class="p-3 bg-beige-100 text-black">$beige-100</div>
+          <div class="p-3 bg-beige-200 text-black">$beige-200</div>
+          <div class="p-3 bg-beige-300 text-black">$beige-300</div>
+          <div class="p-3 bg-beige-400 text-black">$beige-400</div>
+          <div class="p-3 bg-beige-500 text-black">$beige-500</div>
+          <div class="p-3 bg-beige-600 text-black">$beige-600</div>
+          <div class="p-3 bg-beige-700 text-white">$beige-700</div>
+          <div class="p-3 bg-beige-800 text-white">$beige-800</div>
+          <div class="p-3 bg-beige-900 text-white">$beige-900</div>
+        </div>
+
+        <div>
+          <div class="p-3 mb-2 position-relative bg-gold-500 text-black">
+            <strong class="d-block">$gold</strong>
+            #ffbc24
+          </div>
+          <div class="p-3 bg-gold-100 text-black">$gold-100</div>
+          <div class="p-3 bg-gold-200 text-black">$gold-200</div>
+          <div class="p-3 bg-gold-300 text-black">$gold-300</div>
+          <div class="p-3 bg-gold-400 text-black">$gold-400</div>
+          <div class="p-3 bg-gold-500 text-black">$gold-500</div>
+          <div class="p-3 bg-gold-600 text-black">$gold-600</div>
+          <div class="p-3 bg-gold-700 text-black">$gold-700</div>
+          <div class="p-3 bg-gold-800 text-white">$gold-800</div>
+          <div class="p-3 bg-gold-900 text-white">$gold-900</div>
         </div>
       </div>
 
