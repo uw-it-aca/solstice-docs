22f45bb050385e44b5175eb55db4a1e839bb3708
Diego Bejarano <bexar@uw.edu>
2024-04-17 12:40:21 -0600

    adds content to new alerts page in nav

--- src/pages/patterns/messaging/alert.vue
+++ src/pages/patterns/messaging/alert.vue
@@ -15,11 +15,16 @@
         </ol>
       </nav>
     </template>
+    <template #head>
+      <h1 class="fw-bold">{{ pageTitle }}</h1>
+      <hr class="w-50 d-inline-block" />
+    </template>
     <template #lead>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium ad,
-      dolores repudiandae natus dolorem recusandae! Incidunt consequatur tenetur
-      ad totam, placeat ipsa explicabo, eaque id blanditiis libero culpa veniam
-      aliquam!
+      Alerts are semantic messages that intentionally disrupt a user's workflow
+      to notify them about issues that require action or provide information
+      relevant to a particular task or page section. For quick, non-critical
+      messages that disappear on their own, use a
+      <a href="/elements/toasts">Toast</a>.
     </template>
     <template #toc>
       <PageContents>
@@ -27,30 +32,58 @@
           <ul class="list-unstyled m-0">
             <li>
               <a
-                href="#heading1"
+                href="#variants"
                 class="px-2 py-1 text-muted link-underline link-underline-opacity-0 rounded d-block bg-body-tertiary-hover"
-                >Heading 1</a
+                >Variants</a
               >
             </li>
             <li>
               <a
-                href="#heading2"
+                href="#options"
                 class="px-2 py-1 text-muted link-underline link-underline-opacity-0 rounded d-block bg-body-tertiary-hover"
-                >Heading 2</a
+                >Options</a
               >
             </li>
             <li>
               <a
-                href="#heading3"
-                class="px-2 py-1 text-muted link-underline link-underline-opacity-0 rounded d-block bg-body-tertiary-hover"
-                >Heading 3</a
+                href="#anatomy"
+                class="px-2 py-1 text-muted text-decoration-none rounded d-block bg-body-tertiary-hover"
+                >Anatomy</a
               >
             </li>
             <li>
               <a
-                href="#heading4"
-                class="px-2 py-1 text-muted link-underline link-underline-opacity-0 rounded d-block bg-body-tertiary-hover"
-                >Heading 4</a
+                href="#placement"
+                class="px-2 py-1 text-muted text-decoration-none rounded d-block bg-body-tertiary-hover"
+                >Placement</a
+              >
+            </li>
+            <li>
+              <a
+                href="#states"
+                class="px-2 py-1 text-muted text-decoration-none rounded d-block bg-body-tertiary-hover"
+                >States</a
+              >
+            </li>
+            <li>
+              <a
+                href="#practices"
+                class="px-2 py-1 text-muted text-decoration-none rounded d-block bg-body-tertiary-hover"
+                >Best Practices</a
+              >
+            </li>
+            <li>
+              <a
+                href="#implementation"
+                class="px-2 py-1 text-muted text-decoration-none rounded d-block bg-body-tertiary-hover"
+                >Implementation</a
+              >
+            </li>
+            <li>
+              <a
+                href="#changlog"
+                class="px-2 py-1 text-muted text-decoration-none rounded d-block bg-body-tertiary-hover"
+                >Changelog</a
               >
             </li>
           </ul>
@@ -58,50 +91,157 @@
       </PageContents>
     </template>
     <template #content>
-      <h2 id="heading1">Heading1 (h2)</h2>
+      <h2 id="variants">Variants</h2>
+      <h3>Critical</h3>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam fuga
-        corporis totam eligendi molestiae ex aperiam soluta, consectetur
-        accusamus accusantium officia praesentium, magni numquam sunt minus
-        architecto libero enim tempore!
+        Alerts with the <code>critical</code> variant are of the highest
+        importance, and communicate that an item needs immediate attention, that
+        a process is blocked, or that there has been an error. Whenever
+        possible, <code>critical</code> Alerts should include a call to action
+        to help the user resolve the issue.
       </p>
-
-      <h2 id="heading2">Heading2 (h2)</h2>
+      <div class="mb-5">
+        <CodeBlock>
+          <template #preview>
+            <p>Critical alert example goes here</p>
+          </template>
+          <template #markup>
+            <pre class="language-html">
+<code>&lt;sol-alert variant="critical" has-cta="true" cta-url="[url]"
+  cta-label="View statement">You have a balance that is past due.
+&lt;/sol-alert>
+</code></pre>
+          </template>
+        </CodeBlock>
+      </div>
+      <h3>Warning</h3>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quibusdam
-        doloremque quaerat nulla enim, magni ut pariatur est dolorum ullam sint
-        rerum odio hic perspiciatis, commodi adipisci, vero sapiente impedit?
+        Alerts with the <code>warning</code> variant communicate items or
+        statuses that may have future consequences and require attention, but
+        are not currently causing an error or blocking a process. Whenever
+        possible, <code>warning</code>
+        Alerts should include a call to action to help the user learn more about
+        the issue.
       </p>
-
-      <h2 id="heading3">Heading3 (h2)</h2>
+      <div class="mb-5">
+        <CodeBlock>
+          <template #preview>
+            <p>Warning alert example goes here</p>
+          </template>
+          <template #markup>
+            <pre class="language-html">
+<code>&lt;sol-alert variant="warning" has-cta="true" cta-url="[url]"
+  cta-label="Go to ">Class meeting times overlap. Adjust your schedule to avoid conflicts.
+&lt;/sol-alert>
+</code></pre>
+          </template>
+        </CodeBlock>
+      </div>
+      <h3>Info</h3>
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi, sit, quo
-        ab pariatur, recusandae obcaecati necessitatibus doloribus ad a dolor
-        minus saepe? Quaerat cupiditate ipsa ut nulla nostrum ex aut!
+        Alerts with the <code>info</code> variant communicate useful (but
+        non-critical) information to help the user better understand the current
+        context, such as announcements about upcoming processes.
       </p>
-
-      <h2 id="heading4">Heading3 (h2)</h2>
+      <div class="mb-5">
+        <CodeBlock>
+          <template #preview>
+            <p>Info alert example goes here</p>
+          </template>
+          <template #markup>
+            <pre class="language-html">
+<code>&lt;sol-alert variant="info" is-dismissable="true">
+  Registration opens at 6:00AM.
+&lt;/sol-alert>
+</code></pre>
+          </template>
+        </CodeBlock>
+      </div>
+      <h2 id="options">Options</h2>
+      <h3>Call to Action</h3>
       <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati
-        facilis, ipsum dignissimos quibusdam consectetur dolores soluta nemo
-        placeat totam dolorem suscipit est sunt eum, debitis exercitationem
-        voluptate ducimus repellat necessitatibus?
+        Whenever possible, Alerts should provide direct access to relevant
+        actions needed to resolve them. Do this by adding a call to action with
+        <code>has-cta="true"</code>, and specifying a URL with
+        <code>cta-url</code> and a label with <code>cta-label</code>. The CTA
+        should provide a link to an appropriate page where the issue can be
+        resolved. Alerts may only have one call to action including one relevant
+        link.
       </p>
+      <p>
+        In general, <code>critical</code> and <code>warning</code> Alerts should
+        include calls to action whenever possible.
+      </p>
+      <div class="mb-5">
+        <CodeBlock>
+          <template #preview>
+            <p>Call to action alert here</p>
+          </template>
+          <template #markup>
+            <pre class="language-html">
+<code>&lt;sol-alert variant="warning" has-cta="true" cta-url="[url]" cta-
+  label="Call to action"> This Alert requires further action.
+&lt;/sol-alert>
+</code></pre>
+          </template>
+        </CodeBlock>
+      </div>
+      <h3>Dismiss button</h3>
+      <p>
+        By default, Alerts are not dismissible from the page. They will remain
+        in the interface until the issue is resolved or is no longer relevant.
+        For Alerts with non-critical information, particularly info Alerts such
+        as announcements, dismissibility may be enabled. When
+        <code>is-dismissible="true"</code>, an 'x' icon will allow users to
+        remove it from the page.
+      </p>
+      <p>
+        In general, <code>critical</code> and <code>warning</code> Alerts should
+        not be dismissible.
+      </p>
+      <div class="mb-5">
+        <CodeBlock>
+          <template #preview>
+            <p>Dismissable alert goes here</p>
+          </template>
+          <template #markup>
+            <pre class="language-html">
+<code>&lt;sol-alert variant="info" is-dismissible="true">This Alert is dismissible
+&lt;/sol-alert>
+</code></pre>
+          </template>
+        </CodeBlock>
+      </div>
+      <h2 id="anatomy">Anatomy</h2>
+      <h3>Size</h3>
+      <h3>Text</h3>
+      <h3>Color</h3>
+      <h2 id="placement">Placement</h2>
+      <h3>Position</h3>
+      <h3>Stacking</h3>
+      <h2 id="states">States</h2>
+      <h2 id="practice">Best Practices</h2>
+      <h3>Use Alerts for important, temporary messages</h3>
+      <h3>Help the user resolve issues with a call to action</h3>
+      <h3>Be concise and link to additional information</h3>
+      <h3>Assistive technology & keyboard functionality</h3>
+      <h2 id="implementation">Implementation</h2>
+      <h2 id="changelog">Changelog</h2>
     </template>
-    <template #author>Charlon Palacay</template>
+    <template #author>Diego Bejarano</template>
   </Layout>
 </template>
 
 <script>
 import Layout from "@/layout.vue";
-// import CodeBlock from "@/components/CodeBlock.vue";
+import CodeBlock from "@/components/CodeBlock.vue";
 // import UsageBlock from "@/components/UsageBlock.vue";
 import PageContents from "@/components/PageContents.vue";
 
 export default {
   name: "DocsElementsAlert" /* example: Docs--Folder--ComponentName */,
   inject: ["mq"],
-  components: { Layout, /* CodeBlock, UsageBlock, */ PageContents },
+  components: { Layout, CodeBlock, /*  UsageBlock, */ PageContents },
   data() {
     return {
       pageTitle: "Alert",
