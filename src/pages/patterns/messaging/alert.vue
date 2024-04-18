4657788b1dbd3aac1e7ec069a1d37afa34054c1a
Diego Bejarano <bexar@uw.edu>
2024-04-18 10:19:04 -0600

    adds alerts content

--- src/pages/patterns/messaging/alert.vue
+++ src/pages/patterns/messaging/alert.vue
@@ -213,19 +213,213 @@
         </CodeBlock>
       </div>
       <h2 id="anatomy">Anatomy</h2>
-      <h3>Size</h3>
+      <h3>Sizing</h3>
+      <p>
+        Alerts should occupy the full width of their parent container, usually a
+        section or module on a page.
+      </p>
       <h3>Text</h3>
+      <p>Alerts contain a message which should communicate the following:</p>
+      <ul>
+        <li>
+          Why the Alert is there, or what issue exists that needs attention.
+        </li>
+        <li>
+          If the Alert requires user action, how to resolve it (with a CTA if
+          applicable; see below).
+        </li>
+      </ul>
+
       <h3>Color</h3>
+      <p>
+        Alerts always carry a semantic meaning with a corresponding color:
+        <code>critical</code> (red), <code>warning</code> (yellow), or
+        <code>info</code> (blue). Refer to the
+        <a href="#">Solstice Semantic Color Palette</a>
+        for appropriate values.
+      </p>
       <h2 id="placement">Placement</h2>
       <h3>Position</h3>
+      <p>
+        Alerts should appear in the section in which they are relevant, directly
+        under the appropriate heading. In most cases, this will be directly
+        under a section header (when the Alert applies to the whole section) or
+        under a tab group (when the Alert applies only to the tab selected).
+      </p>
+      <UsageBlock
+        ><template #text
+          >Place Alerts at the top of the section in which it's relevant. If the
+          Alert is only relevant to a particular subsection or tab, place it at
+          the top of that subsection or tab.</template
+        ><template #preview
+          ><div class="border rounded-3 p-3 bg-body-tertiary">
+            <img
+              src="/src/assets/images/banner-position-1.png"
+            /></div></template
+      ></UsageBlock>
+      <UsageBlock variant="dont"
+        ><template #text
+          >Don't place Alerts below other content in a section or subsection,
+          where they might be more easily missed.</template
+        ><template #preview
+          ><div class="border rounded-3 p-3 bg-body-tertiary">
+            <img
+              src="/src/assets/images/banner-position-2.png"
+            /></div></template
+      ></UsageBlock>
       <h3>Stacking</h3>
+      <p>
+        In cases where there are multiple Alerts shown at the same time, they
+        should stack vertically with the highest priority Alert on top (from top
+        to bottom: <code>critical</code> Alerts, <code>warning</code> Alerts,
+        <code>info</code> Alerts). In this situation, Alerts should remain at
+        the top of the most relevant section and push other content down.
+      </p>
       <h2 id="states">States</h2>
+      <p>
+        Alerts, when visible, only have one state. When an Alert has a call to
+        action, the embedded outline button element should have a functional
+        hover state.
+      </p>
       <h2 id="practice">Best Practices</h2>
       <h3>Use Alerts for important, temporary messages</h3>
+      <p>
+        Alerts draw attention and disrupt the user's workflow. They are
+        persistent (including across page loads), and will remain on the page
+        until they are resolved or are not longer relevant. For Alerts that are
+        dismissible, they will remain on the page until the user clicks the
+        dismiss button.
+      </p>
+      <p>Use Alerts for messages like:</p>
+      <ul>
+        <li>Warnings and errors</li>
+        <li>When further action is required from the user</li>
+        <li>
+          For timely, important information or announcements (make these
+          dismissible)
+        </li>
+      </ul>
+      <UsageBlock
+        ><template #text>Use Alerts for temporary, timely messages.</template
+        ><template #preview
+          ><div class="border rounded-3 p-3 bg-body-tertiary"></div></template
+      ></UsageBlock>
+      <UsageBlock variant="dont"
+        ><template #text
+          >Don't use Alerts to call out permanent content on the page.</template
+        ><template #preview
+          ><div class="border rounded-3 p-3 bg-body-tertiary"></div></template
+      ></UsageBlock>
       <h3>Help the user resolve issues with a call to action</h3>
+      <p>
+        Give enough detail to convey the message, and use a call to action to
+        direct the user to a page where they can resolve the issue if possible
+        (especially for <code>critical</code> and <code>warning</code> Alerts).
+      </p>
+      <UsageBlock
+        ><template #text
+          >Explain what caused the Alert and direct the user to the right place
+          to resolve it.</template
+        ><template #preview
+          ><div class="border rounded-3 p-3 bg-body-tertiary"></div></template
+      ></UsageBlock>
+      <UsageBlock variant="dont"
+        ><template #text
+          >Don't use vague language or error codes that are not actionable to
+          the user.</template
+        ><template #preview
+          ><div class="border rounded-3 p-3 bg-body-tertiary"></div></template
+      ></UsageBlock>
       <h3>Be concise and link to additional information</h3>
+      <p>
+        Alerts should be no more than 1-3 sentences, but can expand to fit any
+        amount of text. Give enough detail to convey the message, and use a call
+        to action to direct the user to a page where they can learn more about
+        the issue.
+      </p>
+      <UsageBlock
+        ><template #text
+          >Keep Alert message concise and link to a relevant page for more
+          details.</template
+        ><template #preview
+          ><div class="border rounded-3 p-3 bg-body-tertiary"></div></template
+      ></UsageBlock>
+      <UsageBlock variant="dont"
+        ><template #text
+          >Don't use multiple, long sentences or mention multiple
+          issues.</template
+        ><template #preview
+          ><div class="border rounded-3 p-3 bg-body-tertiary"></div></template
+      ></UsageBlock>
       <h3>Assistive technology & keyboard functionality</h3>
-      <h2 id="implementation">Implementation</h2>
+      <p>
+        Alerts include the ARIA <code>role="alert"</code> by default, and should
+        automatically be readable by screen readers and other assistive
+        technologies. Ensure that the component works with these technologies as
+        expected.
+      </p>
+
+      <p>
+        Alerts with calls to action or dismiss buttons should be completely
+        navigable by keyboard alone; the Alert itself will not receive focus,
+        but the interactive children must. Check to make sure that any
+        interactive elements are accessible via keyboard and that they behave as
+        expected.
+      </p>
+
+      <p>
+        When Alerts are dismissed or resolved, they disappear from the page
+        structure (read <a href="#">Bootstrap's documentation</a> on this). The
+        Alert component is configured to set focus to the top of the section in
+        which the Alert was present. Check to make sure that after an Alert is
+        dismissed, focus is set to a predictable location.
+      </p>
+      <h2 id="implementation">Technical Implementation</h2>
+      <table class="table my-5">
+        <thead>
+          <tr>
+            <th scope="col">Property</th>
+            <th scope="col">Values</th>
+            <th scope="col">Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>variant</td>
+            <td>
+              <code>critical</code> / <code>warning</code> / <code>info</code>
+            </td>
+            <td>
+              string<br />Indicates semantic function (applies color and icon)
+            </td>
+          </tr>
+          <tr>
+            <td>is-dismissable</td>
+            <td><code>true</code> / <code>false</code></td>
+            <td>boolean<br />Adds close icon button to dismiss element.</td>
+          </tr>
+          <tr>
+            <td>has-cta</td>
+            <td><code>true</code> / <code>false</code></td>
+            <td>boolean<br />Adds CTA link with button appearance.</td>
+          </tr>
+          <tr>
+            <td>cta-url</td>
+            <td></td>
+            <td>
+              string<br />Destination URL for CTA [when
+              <code>has-cta="true"].</code>
+            </td>
+          </tr>
+          <tr>
+            <td>cta-label</td>
+            <td></td>
+            <td>
+              string<br />Label for for CTA [when <code>has-cta="true"].</code>
+            </td>
+          </tr>
+        </tbody>
+      </table>
       <h2 id="changelog">Changelog</h2>
     </template>
     <template #author>Diego Bejarano</template>
@@ -235,13 +429,13 @@
 <script>
 import Layout from "@/layout.vue";
 import CodeBlock from "@/components/CodeBlock.vue";
-// import UsageBlock from "@/components/UsageBlock.vue";
 import PageContents from "@/components/PageContents.vue";
+import UsageBlock from "@/components/UsageBlock.vue";
 
 export default {
   name: "DocsElementsAlert" /* example: Docs--Folder--ComponentName */,
   inject: ["mq"],
-  components: { Layout, CodeBlock, /*  UsageBlock, */ PageContents },
+  components: { Layout, CodeBlock, PageContents, UsageBlock },
   data() {
     return {
       pageTitle: "Alert",
