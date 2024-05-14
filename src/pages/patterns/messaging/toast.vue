a1d7aa7516954abb6fc81becdcafc75fe1db5f25
Diego Bejarano <bexar@uw.edu>
2024-05-14 15:39:42 -0600

    adds content structure for toasts

--- src/pages/patterns/messaging/toast.vue
+++ src/pages/patterns/messaging/toast.vue
@@ -16,10 +16,11 @@
       </nav>
     </template>
     <template #lead>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium ad,
-      dolores repudiandae natus dolorem recusandae! Incidunt consequatur tenetur
-      ad totam, placeat ipsa explicabo, eaque id blanditiis libero culpa veniam
-      aliquam!
+      Toasts give timely feedback about non-critical information that does not
+      require further action. In Solstice, Toasts are primarily used to
+      communicate success and confirmation messages with minimal disruption, and
+      they dismiss automatically. For intentionally disruptive messages
+      communicating critical information, use an <a href="alert">Alert</a>.
     </template>
     <template #toc>
       <PageContents>
@@ -27,30 +28,44 @@
           <ul class="list-unstyled m-0">
             <li>
               <a
-                href="#heading1"
+                href="#variants"
                 class="px-2 py-1 text-muted link-underline link-underline-opacity-0 rounded d-block bg-body-tertiary-hover"
-                >Heading 1</a
+                >Variants</a
               >
             </li>
             <li>
               <a
-                href="#heading2"
+                href="#anatomy"
                 class="px-2 py-1 text-muted link-underline link-underline-opacity-0 rounded d-block bg-body-tertiary-hover"
-                >Heading 2</a
+                >Anatomy</a
               >
             </li>
             <li>
               <a
-                href="#heading3"
+                href="#placement"
                 class="px-2 py-1 text-muted link-underline link-underline-opacity-0 rounded d-block bg-body-tertiary-hover"
-                >Heading 3</a
+                >Placement</a
               >
             </li>
             <li>
               <a
-                href="#heading4"
+                href="#states"
                 class="px-2 py-1 text-muted link-underline link-underline-opacity-0 rounded d-block bg-body-tertiary-hover"
-                >Heading 4</a
+                >States</a
+              >
+            </li>
+            <li>
+              <a
+                href="#practices"
+                class="px-2 py-1 text-muted link-underline link-underline-opacity-0 rounded d-block bg-body-tertiary-hover"
+                >Best Practices</a
+              >
+            </li>
+            <li>
+              <a
+                href="#implementation"
+                class="px-2 py-1 text-muted link-underline link-underline-opacity-0 rounded d-block bg-body-tertiary-hover"
+                >Implementation</a
               >
             </li>
           </ul>
@@ -58,29 +73,101 @@
       </PageContents>
     </template>
     <template #content>
-      <h2 id="heading1">Heading1 (h2)</h2>
+      <h2 id="variants">Variants</h2>
+      <h3>Success</h3>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam fuga
-        corporis totam eligendi molestiae ex aperiam soluta, consectetur
-        accusamus accusantium officia praesentium, magni numquam sunt minus
-        architecto libero enim tempore!
+        Toasts with the <code>success</code> variant confirm that simple user
+        actions have completed as intended, and do not require additional
+        attention. This variant carries the semantic color green.
       </p>
-
-      <h2 id="heading2">Heading2 (h2)</h2>
+      <div class="mb-5">
+        <CodeBlock>
+          <template #preview>
+            <p>Message here</p>
+          </template>
+          <template #markup>
+            <pre class="language-html">
+<code>&lt;sol-toast variant="success">
+  Added to plan
+&lt;/sol-toast>
+</code></pre>
+          </template>
+        </CodeBlock>
+      </div>
+      <h3>Neutral</h3>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quibusdam
-        doloremque quaerat nulla enim, magni ut pariatur est dolorum ullam sint
-        rerum odio hic perspiciatis, commodi adipisci, vero sapiente impedit?
+        Toasts with the <code>neutral</code> variant can be used in rare cases
+        to convey timely, non-critical feedback that does not make sense as a
+        <code>success</code>. Rather than a semantic color, this variant appears
+        in gray.
       </p>
+      <div class="mb-5">
+        <CodeBlock>
+          <template #preview>
+            <p>Internet disconnect</p>
+          </template>
+          <template #markup>
+            <pre class="language-html">
+<code>&lt;sol-toast variant="success">
+  Internet disconnect
+&lt;/sol-toast>
+</code></pre>
+          </template>
+        </CodeBlock>
+      </div>
 
-      <h2 id="heading3">Heading3 (h2)</h2>
+      <h2 id="anatomy">Anatomy</h2>
+      <h3>Sizing</h3>
+      <p>
+        Toasts occupy a fixed width. By default, their height accommodates one
+        line of text; Toasts can expand to fit additional lines, but text length
+        should be kept to one line when possible. Since Toasts sit above page
+        content, their size neither affects nor is affected by other elements in
+        the markup.
+      </p>
+      <h3>Color</h3>
+      <p>
+        Alerts always carry a semantic meaning with a corresponding color:
+        <code>critical</code> (red), <code>warning</code> (yellow), or
+        <code>info</code> (blue). Refer to the Solstice
+        <a href="color">Semantic Color Palette</a>
+        for appropriate values.
+      </p>
+      <h3>Dark Mode Support</h3>
+      <p>
+        Alerts must maintain proper contrast in order to be readable in both
+        light and dark modes. By default, the component will update to
+        appropriate colors within the semantic color palette.
+      </p>
+      <p>
+        Below are the primitive color values (100-900) for each of the elements
+        within an Alert. The <code>success</code> colors are shown as an
+        example, but the primitive values apply to each of the semantic colors.
+      </p>
+      <h2 id="placement">Placement</h2>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi, sit, quo
         ab pariatur, recusandae obcaecati necessitatibus doloribus ad a dolor
         minus saepe? Quaerat cupiditate ipsa ut nulla nostrum ex aut!
       </p>
 
-      <h2 id="heading4">Heading3 (h2)</h2>
+      <h2 id="states">States</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati
+        facilis, ipsum dignissimos quibusdam consectetur dolores soluta nemo
+        placeat totam dolorem suscipit est sunt eum, debitis exercitationem
+        voluptate ducimus repellat necessitatibus?
+      </p>
+
+      <h2 id="practices">Best Practices</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati
+        facilis, ipsum dignissimos quibusdam consectetur dolores soluta nemo
+        placeat totam dolorem suscipit est sunt eum, debitis exercitationem
+        voluptate ducimus repellat necessitatibus?
+      </p>
+
+      <h2 id="implementation">Technical Implementation</h2>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati
         facilis, ipsum dignissimos quibusdam consectetur dolores soluta nemo
@@ -94,14 +181,14 @@
 
 <script>
 import Layout from "@/layout.vue";
-// import CodeBlock from "@/components/CodeBlock.vue";
-// import UsageBlock from "@/components/UsageBlock.vue";
+import CodeBlock from "@/components/CodeBlock.vue";
+import UsageBlock from "@/components/UsageBlock.vue";
 import PageContents from "@/components/PageContents.vue";
 
 export default {
   name: "DocsTemplatesComponentName" /* example: Docs--Folder--ComponentName */,
   inject: ["mq"],
-  components: { Layout, /* CodeBlock, UsageBlock, */ PageContents },
+  components: { Layout, CodeBlock, UsageBlock, PageContents },
   data() {
     return {
       pageTitle: "Toast",
