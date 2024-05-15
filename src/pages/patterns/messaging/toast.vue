669f3e07731d43666628b9dd133227ae05b6fdd2
Diego Bejarano <bexar@uw.edu>
2024-05-15 11:06:33 -0600

    adds more toast content

--- src/pages/patterns/messaging/toast.vue
+++ src/pages/patterns/messaging/toast.vue
@@ -135,28 +135,95 @@
       </p>
       <h3>Dark Mode Support</h3>
       <p>
-        Alerts must maintain proper contrast in order to be readable in both
+        Toasts must maintain proper contrast in order to be readable in both
         light and dark modes. By default, the component will update to
         appropriate colors within the semantic color palette.
       </p>
       <p>
         Below are the primitive color values (100-900) for each of the elements
-        within an Alert. The <code>success</code> colors are shown as an
+        within an Toast. The <code>success</code> colors are shown as an
         example, but the primitive values apply to each of the semantic colors.
       </p>
-      <h2 id="placement">Placement</h2>
+      <h3>Icon</h3>
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi, sit, quo
-        ab pariatur, recusandae obcaecati necessitatibus doloribus ad a dolor
-        minus saepe? Quaerat cupiditate ipsa ut nulla nostrum ex aut!
+        Toasts with the <code>success</code> variant contain a corresponding
+        semantic icon. This icon always appears to the left of the text. Since
+        they have no semantic meaning, toasts with the
+        <code>neutral</code> variant do not contain an icon.
       </p>
-
+      <h3>Dismiss Button</h3>
+      <p>
+        While toasts will disappear after a specified time automatically by
+        default, they also contain a button to dismiss them manually. This
+        button (an 'x' icon) always appears to the right of the text.
+      </p>
+      <h3>Shadow</h3>
+      <p>
+        Toasts sit above page content, so they use a shadow to communicate
+        elevation. By default, Toasts use the default shadow from
+        <a href="https://getbootstrap.com/docs/5.3/utilities/shadows/"
+          >Bootstrap's</a
+        >
+        <code>box-shadow</code> utility classes.
+      </p>
+      <h2 id="placement">Placement</h2>
+      <h3>Position</h3>
+      <p>
+        Place Toasts centered across the bottom of the window or viewport, with
+        adequate padding (at least 16px/1rem) above the bottom.
+      </p>
+      <p>
+        On mobile, place Toasts centered across the bottom of the page viewport,
+        above any browser UI.
+      </p>
+      <p>
+        On any given platform (and with as much cross-platform consistency as
+        possible), Toasts should appear in the same place every time so that
+        users know where to expect them.
+      </p>
+      <UsageBlock
+        ><template #text
+          >Always display Toasts in the same place, centered at the bottom of
+          the windowc.</template
+        ><template #preview
+          ><div class="border rounded-3 p-3 bg-body-tertiary">
+            <img
+              class="img-fluid"
+              src="/src/assets/images/toast-asset-placement-do.png"
+            /></div></template
+      ></UsageBlock>
+      <UsageBlock variant="dont"
+        ><template #text
+          >Don't put Toasts in different places throughout the page, where users
+          might not expect them or where they might be more likely to block
+          important content.</template
+        ><template #preview
+          ><div class="border rounded-3 p-3 bg-body-tertiary">
+            <img
+              class="img-fluid"
+              src="/src/assets/images/toast-asset-placement-dont.png"
+            /></div></template
+      ></UsageBlock>
+      <h3>Autohide</h3>
+      <p>
+        By default, the <code>autohide</code> property is true, and Toasts will
+        disappear automatically after 5 seconds. For most simple messages, this
+        should be sufficient. In special cases, <code>autohide</code> can be
+        turned off or the <code>delay</code> can be extended. Do not show a
+        Toast with a <code>delay</code> of less than 5 seconds, as there is a
+        greater chance of it being missed.
+      </p>
+      <h3>Stacking</h3>
+      <p>
+        By default, multiple Toasts stack on top of each other, with the most
+        recent message on top. When a Toast disappears, others move down to
+        occupy the space closest to the bottom of the screen.
+      </p>
+      <img src="">
       <h2 id="states">States</h2>
       <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati
-        facilis, ipsum dignissimos quibusdam consectetur dolores soluta nemo
-        placeat totam dolorem suscipit est sunt eum, debitis exercitationem
-        voluptate ducimus repellat necessitatibus?
+        When visible, Toasts only exist in one state. By default, the dismiss
+        button (an x icon) should have a functional hover state.
       </p>
 
       <h2 id="practices">Best Practices</h2>
