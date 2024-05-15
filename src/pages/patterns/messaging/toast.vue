85a17d16e1301dba3423c56c2763f9eeaa4e969a
Diego Bejarano <bexar@uw.edu>
2024-05-15 12:22:13 -0600

    adds rest of content

--- src/pages/patterns/messaging/toast.vue
+++ src/pages/patterns/messaging/toast.vue
@@ -219,28 +219,159 @@
         recent message on top. When a Toast disappears, others move down to
         occupy the space closest to the bottom of the screen.
       </p>
-      <img src="">
-      <h2 id="states">States</h2>
+      <img
+        class="img-fluid"
+        src="/src/assets/images/toast-asset-stacking-visual.png"
+      />
+      <h2 id="states" class="mt-3">States</h2>
       <p>
         When visible, Toasts only exist in one state. By default, the dismiss
         button (an x icon) should have a functional hover state.
       </p>
 
       <h2 id="practices">Best Practices</h2>
+      <h3>Use Toasts for real-time confirmation and updates</h3>
       <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati
-        facilis, ipsum dignissimos quibusdam consectetur dolores soluta nemo
-        placeat totam dolorem suscipit est sunt eum, debitis exercitationem
-        voluptate ducimus repellat necessitatibus?
+        Use Toasts for non-critical status updates that do not require further
+        action on the part of the user and are not meant to disrupt their
+        workflow. Toasts should provide helpful feedback about what's going on,
+        but if a user misses a Toast, it should not prevent them from completing
+        other tasks on the page.
+      </p>
+      <p>
+        Toasts should primarily be used to confirm the completion of actions
+        (use the success variant). Examples include:
+      </p>
+      <ul>
+        <li>When a course is added to or deleted from MyPlan</li>
+        <li>When grades are successfully submitted</li>
+        <li>When settings are updated</li>
+      </ul>
+      <p>
+        For notifications for which the success variant does not make sense, and
+        when the information is not critical enough to be an Alert, use the
+        neutral variant.
+      </p>
+      <UsageBlock
+        ><template #text
+          >Use Toasts to notify the user that actions have completed
+          successfully.</template
+        ><template #preview
+          ><div class="border rounded-3 p-3 bg-body-tertiary">
+            <img
+              class="img-fluid"
+              src="/src/assets/images/toast-asset-do1.png"
+            /></div></template
+      ></UsageBlock>
+      <UsageBlock variant="dont"
+        ><template #text
+          >Don't use Toasts to communicate critical information like errors that
+          require immediate attention.</template
+        ><template #preview
+          ><div class="border rounded-3 p-3 bg-body-tertiary">
+            <img
+              class="img-fluid"
+              src="/src/assets/images/toast-asset-dont1.png"
+            /></div></template
+      ></UsageBlock>
+
+      <h3>Keep messages to a few words</h3>
+      <p>
+        Toast text should be no more than a few words, and not more than one
+        sentence. The text should communicate a simple message, usually
+        confirming or indicating success with no further action needed.
+      </p>
+      <p>
+        If the message indicates critical information, and requires further
+        action or explanation, consider using an <a href="alert">Alert</a>.
+      </p>
+      <UsageBlock
+        ><template #text
+          >Keep Toast text concise—no more than a few words.</template
+        ><template #preview
+          ><div class="border rounded-3 p-3 bg-body-tertiary">
+            <img
+              class="img-fluid"
+              src="/src/assets/images/toast-asset-do2.png"
+            /></div></template
+      ></UsageBlock>
+      <UsageBlock variant="dont"
+        ><template #text
+          >Don't use long sentences or additional formatting.</template
+        ><template #preview
+          ><div class="border rounded-3 p-3 bg-body-tertiary">
+            <img
+              class="img-fluid"
+              src="/src/assets/images/toast-asset-dont2.png"
+            /></div></template
+      ></UsageBlock>
+      <h3>Assistive technology & keyboard functionality</h3>
+      <p>
+        Toasts must be generated within an existing
+        <code>aria-live</code> region in order to be announced by assistive
+        technologies; <code>aria-live</code> should be set to
+        "<code>polite</code>". Read more about accessible structuring in
+        <a href="https://getbootstrap.com/docs/5.3/components/toasts/"
+          >Bootstrap's</a
+        >
+        Toast documentation.
+      </p>
+      <p>
+        Like all elements, Toasts should be fully usable for those navigating
+        the page by keyboard. Pressing `ESC` should dismiss Toasts.
+      </p>
+      <p>
+        Toasts do not receive focus and therefore should not include any links,
+        buttons, or interactive content (with the exception of the dismiss
+        button, whose function is assumed by the `ESC` shortcut). Additionally,
+        dismissing a Toast (automatically or manually) should not change the
+        user's focus.
       </p>
 
       <h2 id="implementation">Technical Implementation</h2>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati
-        facilis, ipsum dignissimos quibusdam consectetur dolores soluta nemo
-        placeat totam dolorem suscipit est sunt eum, debitis exercitationem
-        voluptate ducimus repellat necessitatibus?
-      </p>
+      <table class="table my-5">
+        <thead>
+          <tr>
+            <th scope="col">Property</th>
+            <th scope="col">Values</th>
+            <th scope="col">Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><code class="text-success">variant</code><br />(required)</td>
+            <td><code>success</code> / <code>neutral</code></td>
+            <td>
+              <code>string</code><br />Indicates semantic function (applies
+              color and icon)
+            </td>
+          </tr>
+          <tr>
+            <td><code class="text-success">animation</code></td>
+            <td><code>true</code> / <code>false</code></td>
+            <td>
+              <code>boolean</code><br />Apply a CSS fade transition to the
+              toast.
+            </td>
+          </tr>
+          <tr>
+            <td><code class="text-success">autohide</code></td>
+            <td><code>true</code> / <code>false</code></td>
+            <td>
+              <code>boolean</code><br />Automatically hide the toast after the
+              delay.
+            </td>
+          </tr>
+          <tr>
+            <td><code class="text-success">delay</code></td>
+            <td><code>5000</code></td>
+            <td>
+              <code>number</code><br />Delay in milliseconds before hiding the
+              toast.
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </template>
     <template #author>Charlon Palacay</template>
   </Layout>
