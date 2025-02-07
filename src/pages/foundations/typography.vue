1cb8bff222871b14cb4aaf26cb1bc3b593fcf5a9
Cayla Snooke <csnooke@uw.edu>
2025-02-06 16:37:20 -0800

    Starting updates to typography documentation

--- src/pages/foundations/typography.vue
+++ src/pages/foundations/typography.vue
@@ -7,51 +7,187 @@
       </BBreadcrumb>
     </template>
     <template #lead>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium ad,
-      dolores repudiandae natus dolorem recusandae! Incidunt consequatur tenetur
-      ad totam, placeat ipsa explicabo, eaque id blanditiis libero culpa veniam
-      aliquam!
+      Font families and a type system, as well as best practices for creating
+      readable and clear hierarchy of content
     </template>
     <template #content>
-      <p class="text-danger">
-        All following content is still TBD
-        <br />
-        <a
-          href="https://www.washington.edu/brand/graphic-elements/font-download/"
-          target="_blank"
-          >https://www.washington.edu/brand/graphic-elements/font-download/</a
-        >
-      </p>
-
-      <h2>Fonts</h2>
-
-      <h3>Body/Content</h3>
-      <h4>Open Sans</h4>
+      <h2 class="display-6 ff-encodesans">Font families</h2>
       <p>
-        Open Sans is the default font-family used by UW. It is typically set at
-        the page body level.
+        The Solstice Design System uses the Encode Sans and Open Sans font
+        families. Use only these provided font families – both have good
+        readability, are widely supported, and are in alignment with the
+        University of Washington’s brand guides.
       </p>
-      <CodeBlock>
+
+      <h3 class="fw-medium ff-encodesans">Encode Sans</h3>
+      <p>Encodes Sans is only used for larger headings. [EXAMPLE TEXT]</p>
+
+      <h3 class="fw-medium ff-encodesans">Open Sans</h3>
+      <p>
+        Open Sans is used for body text, smaller headings, and most UI
+        components (e.g. messaging, form labels). [EXAMPLE TEXT]
+      </p>
+
+      <br />
+      <hr />
+      <h2 class="display-6 ff-encodesans">
+        Usability & accessibility principles
+      </h2>
+      <h3 class="fw-medium ff-encodesans">Usability</h3>
+      <ul>
+        <li>
+          Maintain Consistency Across Pages: Use the same fonts, sizes, and
+          spacing for similar elements (e.g., headings, body text, buttons)
+          across all pages to create a unified experience. Default styles for
+          each HTML element and Bootstrap component are established in the
+          Solstice and Bootstrap stylesheets, you should avoid overriding these
+          default font styles.
+        </li>
+      </ul>
+      <h3 class="fw-medium ff-encodesans">Accessibility</h3>
+      <ul>
+        <li>
+          Ensure all headings on a page are semantically defined in code. Note
+          that clear and semantic headings are also important for good search
+          results (SEO).
+        </li>
+        <li>
+          Ensure your heading hierarchy is clear and logical – don’t skip
+          heading levels.
+        </li>
+        <li>
+          If you emphasize something using large text, it should likely be a
+          semantic HTML heading.
+        </li>
+      </ul>
+
+      <br>
+      <hr>
+
+      <h2 class="display-6 ff-encodesans">Headings</h2>
+      <p>
+        Headings are used to provide structure and clarity to page content
+        through hierarchy. Like outlines or table of contents, they summarize
+        content, communicate levels of importance, and define relationships
+        between content.
+      </p>
+      <h3 class="fw-medium ff-encodesans">Best practices</h3>
+      <ul>
+        <li>
+          Heading text should be directly related to the content it is
+          containing. See Labels page for more guidance.
+        </li>
+        <li>
+          The H1 tag is typically reserved for the page title or main heading,
+          while H2 and H3 tags break up sections and subsections.
+        </li>
+        <li>
+          Use H2 through H6 tags to define peer and parent-child relationships
+          in the content.
+        </li>
+        <li>Avoid having only one subsection heading within a section.</li>
+        <li>Use only the headings styles defined in the type system below.</li>
+      </ul>
+
+      <UsageBlock variant="dont"
+        ><template #text>Don't mix heading styles or semantic tags</template
+        ><template #preview
+          ><div class="border rounded-3 p-3 bg-body-tertiary">
+            <img /></div></template
+      ></UsageBlock>
+      <UsageBlock variant="do"
+        ><template #text
+          >Do have more than one subsection under a heading?</template
+        ><template #preview
+          ><div class="border rounded-3 p-3 bg-body-tertiary">
+            <img /></div></template
+      ></UsageBlock>
+
+      <br>
+      <hr>
+
+      <h2 class="display-6 ff-encodesans">Styling & decoration</h2>
+      <h3 class="fw-medium ff-encodesans">Color</h3>
+      <li>
+        You shouldn’t need to change the color of text, as it is defined in the
+        Solstice style sheet and is an inherited property of HTML elements
+        and/or Solstice and Bootstrap themes.
+      </li>
+      <li>
+        All color choices in the Solstice Design System adhere to the
+        accessibility requirements of UW {link to the UW guide}, further
+        outlined in the
+        <a
+          href="https://www.w3.org/WAI/WCAG21/Understanding/contrast-minimum.html"
+          target="_blank"
+          >WCAG Contrast</a
+        >
+        guide, and are dark mode compatible.
+      </li>
+      <li>
+        We strongly recommend against using branding colors for headings or page
+        titles. Branding colors in headings add unnecessary visual complexity to
+        a page and can be misinterpreted as having a semantic meaning.
+      </li>
+      <li>
+        See the comprehensive
+        <a href="/foundations/color" target="_blank">Color</a> page for more
+        guidance.
+      </li>
+
+      <br />
+      <h3 class="fw-medium ff-encodesans">Decoration</h3>
+      <li>
+        <strong>Bold & italics</strong> – Bold and italic styles are used to draw attention and
+        create emphasis. Italicizing words or strings, however, can decrease
+        readability. Both should be used sparingly so as to not diminish their
+        effectiveness.
+      </li>
+      <li>
+        <strong>Underlining</strong> – Avoid underlining text as it can conflict with link
+        styling and may falsely indicate that text is interactive.
+      </li>
+      <li>
+        <strong>Strikethrough</strong> – Avoid using “strikethrough”. If you need to indicate
+        that there has been a change to content, do so in another way.
+      </li>
+      <li>
+        <strong>Outlining</strong> – Text should never be outlined. Use bold or italic styles to
+        draw attention or create emphasis.
+      </li>
+      <li>
+        <strong>Motion effects</strong> – Do not use motion effects on text. Text should always
+        remain static, unless it is scaling/rearranging due to responsive
+        design.
+      </li>
+
+<br>
+
+      <h3 class="fw-medium ff-encodesans">Case</h3>
+      <li>Use sentence case in most situations and <span class="text-uppercase">AVOID ALL CAPS</span>. <a href="/foundations/labels" target="_blank">Review Labels for more details</a>.</li>
+
+      <br>
+      <hr>
+      <h2 class="display-6 ff-encodesans">Alignment & spacing</h2>
+      <h3 class="fw-medium ff-encodesans">Text alignment</h3>
+      <li>Left-align text within content headings and paragraphs. This supports the “F” shaped scanning pattern that most western readers use. Deviating from this slows readers down.
+</li>
+      <li>Text should never be set to “justify” (aligned to both the left and right margins). This results in inconsistent spacing between words and affects readability.</li>
+
+      <h3 class="fw-medium ff-encodesans">Text layout</h3>
+      <li>Text lines should ideally be limited to 45-85 characters. <a href="/foundations/layout" target="_blank">Review Layout</a> page for specific line length recommendations. </li>
+
+      <h3 class="fw-medium ff-encodesans">Truncation</h3>
+      <li>Avoid truncation, in any text elements. <a href="/foundations/labels" target="_blank">Review Labels for more details</a>.</li>
+
+      <br>
+      <h2 class="display-6 ff-encodesans">two</h2>
+      <h3 class="fw-medium ff-encodesans">three</h3>
+
+
+      <!-- <CodeBlock>
         <template #preview>
-          <div class="ff-open-sans">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet praesentium itaque voluptatum cupiditate repellat ipsa! Quae nam magnam quos veniam iusto cupiditate, mollitia ut ex aliquam quidem. Necessitatibus, consectetur ab.</div>
-        </template>
-        <template #bootstrap>
-          <pre class="language-html">
-<code>&lt;body class="ff-open-sans">Lorem ipsum dolor... Open Sans&lt;/body>
-</code></pre>
-        </template>
-      </CodeBlock>
-
-      <h3>Headings</h3>
-
-      <h4>Encode Sans</h4>
-      <p>Encode Sans is the heading font-family used by UW</p>
-
-      <CodeBlock>
-        <template #preview>
-          <h1 class="ff-encode-sans">
-            Heading... Encode Sans
-          </h1>
+          <h1 class="ff-encode-sans">Heading... Encode Sans</h1>
         </template>
         <template #bootstrap>
           <pre class="language-html">
@@ -65,9 +201,7 @@
 
       <CodeBlock>
         <template #preview>
-          <h1 class="ff-encode-sans">
-            Heading... Roboto
-          </h1>
+          <h1 class="ff-encode-sans">Heading... Roboto</h1>
         </template>
         <template #bootstrap>
           <pre class="language-html">
@@ -81,9 +215,7 @@
 
       <CodeBlock>
         <template #preview>
-          <h1 class="ff-roboto-slab">
-            Heading... Roboto Slab
-          </h1>
+          <h1 class="ff-roboto-slab">Heading... Roboto Slab</h1>
         </template>
         <template #bootstrap>
           <pre class="language-html">
@@ -93,15 +225,11 @@
       </CodeBlock>
 
       <h4>Rozha One</h4>
-      <p>
-        Rozha One is...
-      </p>
+      <p>Rozha One is...</p>
 
       <CodeBlock>
         <template #preview>
-          <h1 class="ff-rozha-one">
-            Heading... Rozha One
-          </h1>
+          <h1 class="ff-rozha-one">Heading... Rozha One</h1>
         </template>
         <template #bootstrap>
           <pre class="language-html">
@@ -113,22 +241,20 @@
       <h2>Sizing</h2>
 
       <p>
-       You can size fonts down using the <code>.small</code> class or the <code>&lt;small></code> element tag. Recommendation is to start with <code>.fs-x</code> classes.
+        You can size fonts down using the <code>.small</code> class or the
+        <code>&lt;small></code> element tag. Recommendation is to start with
+        <code>.fs-x</code> classes.
       </p>
       <CodeBlock>
         <template #preview>
-          <h1 class="ff-encode-sans">
-            Heading (default - 40px)
-          </h1>
+          <h1 class="ff-encode-sans">Heading (default - 40px)</h1>
           <h1 class="ff-encode-sans">
             <small>Heading (element - 35px)</small>
           </h1>
           <h1 class="ff-encode-sans">
             <span class="small">Heading (class - 35px)</span>
           </h1>
-          <h1 class="ff-encode-sans fs-2">
-            Heading (fs-2 - 32px)
-          </h1>
+          <h1 class="ff-encode-sans fs-2">Heading (fs-2 - 32px)</h1>
         </template>
         <template #bootstrap>
           <pre class="language-html">
@@ -146,10 +272,7 @@
 &lt;/h1>
 </code></pre>
         </template>
-      </CodeBlock>
-
-
-
+      </CodeBlock> -->
     </template>
     <template #author>Your Name</template>
   </Layout>
