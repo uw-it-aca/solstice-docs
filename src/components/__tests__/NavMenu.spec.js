e9a7f55437db2bb4700213b701c3292c80a17d2c
Charlon Palacay <charlon@gmail.com>
2025-11-07 16:09:47 -0800

    Update NavMenu tests.

--- src/components/__tests__/NavMenu.spec.js
+++ src/components/__tests__/NavMenu.spec.js
@@ -16,4 +16,64 @@ describe("NavMenu", () => {
     // Assert the rendered text of the component
     expect(wrapper.text()).toContain("Getting Started");
   });
+
+  it("expands the getting started menu when the route contains 'getting-started'", () => {
+    const wrapper = mount(NavMenu, {
+      global: {
+        mocks: {
+          $route: {
+            path: "/getting-started",
+          },
+        },
+      },
+    });
+    // Assert the rendered text of the component
+    const gettingStartedMenu = wrapper.find("#gettingStartedHeading");
+    expect(gettingStartedMenu.attributes("aria-expanded")).toBe("true");
+  });
+
+  it("does not expand the getting started menu when the route does not contain 'getting-started'", () => {
+    const wrapper = mount(NavMenu, {
+      global: {
+        mocks: {
+          $route: {
+            path: "/",
+          },
+        },
+      },
+    });
+    // Assert the rendered text of the component
+    const gettingStartedMenu = wrapper.find("#gettingStartedHeading");
+    expect(gettingStartedMenu.attributes("aria-expanded")).toBe("false");
+  });
+
+  it("expands the content menu when the route contains 'content'", () => {
+    const wrapper = mount(NavMenu, {
+      global: {
+        mocks: {
+          $route: {
+            path: "/content",
+          },
+        },
+      },
+    });
+    // Assert the rendered text of the component
+    const contentMenu = wrapper.find("#contentHeading");
+    expect(contentMenu.attributes("aria-expanded")).toBe("true");
+  });
+
+  it("does not expand the content menu when the route does not contain 'content'", () => {
+    const wrapper = mount(NavMenu, {
+      global: {
+        mocks: {
+          $route: {
+            path: "/",
+          },
+        },
+      },
+    });
+    // Assert the rendered text of the component
+    const contentMenu = wrapper.find("#contentHeading");
+    expect(contentMenu.attributes("aria-expanded")).toBe("false");
+  });
 });
