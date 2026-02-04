aabc970d33cb2ce9115c580263ed70026828904a
Charlon Palacay <charlon@gmail.com>
2026-02-04 12:00:00 -0800

    Reverts to Vite 7.3.1 instead of Rolldown.

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,22 +1,22 @@
 <template>
   <SSidebar :app-name="appName" :app-root-url="'/'" :page-title="pageTitle">
     <template #navigation>
-      <NavMenu/>
+      <NavMenu />
     </template>
     <template #aside>
       <div
-        class="bg-white bg-opacity-10 rounded-3 p-3 small d-flex justify-content-between align-items-center mt-2"
+        class="bg-opacity-10 rounded-3 small d-flex justify-content-between align-items-center mt-2 bg-white p-3"
       >
         <div class="text-light">
           Version
-          <br>
+          <br />
           <a
             href="https://github.com/uw-it-aca/solstice-theme/releases"
             target="_blank"
             class="link-light link-opacity-75 link-opacity-100-hover link-underline-opacity-75 link-underline-opacity-100-hover"
             >solstice-theme: 1.0.7</a
           >
-          <br>
+          <br />
           <a
             href="https://github.com/uw-it-aca/solstice-vue/releases"
             target="_blank"
@@ -25,42 +25,40 @@
           >
         </div>
         <div class="text-light">
-          <SColorMode color-class="text-white"/>
+          <SColorMode color-class="text-white" />
         </div>
       </div>
     </template>
     <template #main>
       <div v-if="$slots.breadcrumb" class="row">
         <div class="col">
-          <slot name="breadcrumb"/>
+          <slot name="breadcrumb" />
         </div>
       </div>
 
-      <div v-if="$slots.lead" class="row">
-        <div class="col-9">
-          <h1 class="fw-bold ff-encode-sans">{{ pageTitle }}</h1>
-          <p class="lead text-body-secondary">
-            <slot name="lead">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-              maiores quo consequatur, minima minus suscipit doloribus quos
-              dicta excepturi porro obcaecati, dignissimos rerum consectetur ea
-              dolores vero sint. Porro, quaerat.
-            </slot>
-          </p>
-          <hr class="w-50 d-inline-block">
-        </div>
+      <div v-if="$slots.lead">
+        <h1 class="fw-bold ff-encode-sans">{{ pageTitle }}</h1>
+        <p class="lead text-body-secondary" style="max-width: 85ch;">
+          <slot name="lead">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            maiores quo consequatur, minima minus suscipit doloribus quos dicta
+            excepturi porro obcaecati, dignissimos rerum consectetur ea dolores
+            vero sint. Porro, quaerat.
+          </slot>
+        </p>
+        <hr class="d-inline-block w-50" />
       </div>
 
       <div class="row">
         <div :class="[mq.xlMinus || !$slots['toc'] ? 'col' : 'col-9']">
           <div v-if="$slots['toc'] && mq.xlMinus" class="mb-5">
-            <slot name="toc"/>
+            <slot name="toc" />
           </div>
-          <slot name="content"/>
+          <slot name="content" />
 
-          <div v-if="$slots.author" class="py-1 small">
+          <div v-if="$slots.author" class="small py-1">
             <div class="text-muted">
-              Last updated by: <slot name="author"/>
+              Last updated by: <slot name="author" />
             </div>
           </div>
         </div>
